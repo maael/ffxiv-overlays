f623a17fb1d6356b1f8d3aaaa0ff0caded603206
Matthew Elphick <matthew.elphick@threadsstyling.com>
2019-07-23 14:11:35 +0100

    fix: 🐛 Properly handle pathing for github pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
   exportPathMap: function () {
     return {
       '/': { page: '/' },
-      '/t/base': { page: '/t/base' }
+      '/t/base': { page: '/t/base' },
+      '/t/base': { page: '/t/base', query: {simulator: true} }
     }
   },
+  assetPrefix: !debug ? '/ffxiv-overlays' : ''
 }
