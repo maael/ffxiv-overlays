3b962ffff2968b18cd325b95a2b8d3257d9c89cd
Matthew Elphick <matthew.elphick@threadsstyling.com>
2019-07-26 08:23:56 +0100

    Deploy Next.js to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/t/base.js.a5d6995dc9eb6200ad79.hot-update.js
@@ -0,0 +1,66 @@
+webpackHotUpdate("static/development/pages/t/base.js",{
+
+/***/ "./util/createEncounter.ts":
+/*!*********************************!*\
+  !*** ./util/createEncounter.ts ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return {
+    "n": "\n",
+    "t": "\t",
+    "title": "Test Encounter",
+    "duration": "00:00",
+    "DURATION": "291",
+    "damage": "0",
+    "damage-m": "0.03",
+    "DAMAGE-k": "32",
+    "DAMAGE-m": "0",
+    "dps": "109.51",
+    "DPS": "110",
+    "DPS-k": "0",
+    "encdps": "109.51",
+    "ENCDPS": "110",
+    "ENCDPS-k": "0",
+    "hits": "856",
+    "crithits": "86",
+    "crithit%": "0%",
+    "misses": "3",
+    "hitfailed": "0",
+    "swings": "859",
+    "tohit": "---",
+    "TOHIT": "---",
+    "maxhit": "Nobono Nobo-Fire-249",
+    "MAXHIT": "Nobono Nobo-249",
+    "healed": "181",
+    "enchps": "0.62",
+    "ENCHPS": "1",
+    "ENCHPS-k": "0",
+    "critheals": "86",
+    "critheal%": "0%",
+    "heals": "3",
+    "cures": "0",
+    "maxheal": "Dodd Himself-Life Surge-70",
+    "MAXHEAL": "Dodd Himself-70",
+    "maxhealward": "Dodd Himself-Life Surge-70",
+    "MAXHEALWARD": "Dodd Himself-70",
+    "damagetaken": "4467",
+    "healstaken": "181",
+    "powerdrain": "0",
+    "powerheal": "0",
+    "kills": "47",
+    "deaths": "1",
+    "Last10DPS": "100",
+    "Last30DPS": "",
+    "Last60DPS": ""
+  };
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=base.js.a5d6995dc9eb6200ad79.hot-update.js.map
