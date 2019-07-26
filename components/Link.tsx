83a21c2e785ffa4735c6f6aaa5821382e63d440b
Matthew Elphick <matthew.elphick@threadsstyling.com>
2019-07-26 10:04:35 +0100

    fix: 🐛 Only prepend backend url if link not external

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
 
 export default ({href, children, ...props}) => (
-  <Link href={href} as={`${process.env.BACKEND_URL}${href}`} {...props}>{typeof children === 'string' ? <a>{children}</a> : children}</Link>
+  <Link href={href} as={`${!href.startsWith('http') && !href.startsWith('//') ? process.env.BACKEND_URL : ''}${href}`} {...props}>{typeof children === 'string' ? <a>{children}</a> : children}</Link>
 );
