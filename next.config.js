7c35e2cd4f6b8900e3a40ca34b7038a60e5a5a6b
Matthew Elphick <matthew.elphick@threadsstyling.com>
2019-07-28 16:16:15 +0100

    feat: 🎸 Autodiscovery of themes in /t/ folder

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
+const fs = require('fs')
+const path = require('path')
 const debug = process.env.NODE_ENV !== 'production'
+
+const themes = fs.readdirSync(path.join(__dirname, 'pages', 't'))
+  .map((n) => `/t/${n.split('.').slice(0, -1).join('-')}`)
+  .reduce((ob, page) => ({...ob, [page]: {page}}), {})
 
 module.exports = {
   exportPathMap: function () {
     return {
       '/': { page: '/' },
-      '/t/base': { page: '/t/base' },
-      '/t/minidps': { page: '/t/minidps' },
-      '/t/sparky': { page: '/t/sparky' },
+      ...themes
     }
   },
   assetPrefix: !debug ? '/ffxiv-overlays' : ''
