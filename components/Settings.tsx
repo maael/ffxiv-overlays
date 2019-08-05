0ebc49fdc470c183234a7b3c5d64f1e34e3d06ed
Matthew Elphick <matthew.elphick@threadsstyling.com>
2019-08-05 11:36:28 +0100

    feat: 🎸 General settings investigation

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -11,6 +11,8 @@ export interface State <T extends object> {
   test: string;
 };
 
+export const settingsContentId = 'settings-content';
+
 export default class Settings <T extends object> extends React.Component<Props, State<T>> {
   state: State<T> = {
     isOpen: false,
@@ -28,8 +30,9 @@ export default class Settings <T extends object> extends React.Component<Props, 
   render () {
     const {children, themeKey} = this.props;
     const {isOpen} = this.state;
-    return isOpen ? (
-      <div style={{background: 'rgba(100, 100, 100, 0.5)'}}>
+    return (
+      <>
+      <div style={{background: 'rgba(100, 100, 100, 0.5)', display: isOpen ? 'initial' : 'none'}}>
         Settings
         <input type='text' onChange={({target}) => {
           this.setState({test: target.value});
@@ -39,8 +42,11 @@ export default class Settings <T extends object> extends React.Component<Props, 
             console.error('localstorage e', e);
           }
         }} value={this.state.test} />
+        <div id={settingsContentId}>Loading...</div>
         <button onClick={() => this.setState({isOpen: false})}>Close</button>
       </div>
-    ) : <div onDoubleClick={() => this.setState({isOpen: true})}>{children}</div>;
+      {!isOpen ? <div onDoubleClick={() => this.setState({isOpen: true})}>{children}</div> : null}
+      </>
+    )
   }
 }
--- components/SettingsPanel.tsx
+++ components/SettingsPanel.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import {createPortal} from 'react-dom';
+import {settingsContentId} from './Settings';
 
 export default class SettingsPanel extends React.Component {
+  el: HTMLElement;
+  constructor (p, c) {
+    super(p, c);
+    if (typeof document === 'undefined') return;
+    this.el = document.createElement('div');
+  }
   render () {
-    return (<div>{this.props.children}</div>);
+    if (typeof document === 'undefined') return null;
+    const settingsContainer = document.getElementById(settingsContentId);
+    return settingsContainer
+      ? createPortal(<div>Hey:{this.props.children}</div>, settingsContainer)
+      : null;
   }
 }
