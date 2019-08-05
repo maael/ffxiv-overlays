616750672c93ceb3d2eac3914f8226c26b2f4085
Matthew Elphick <matthew.elphick@threadsstyling.com>
2019-08-05 11:36:09 +0100

    fix: 🐛 Make DragCorner stand out less

--- components/DragCorner.tsx
+++ components/DragCorner.tsx
@@ -1,3 +1,3 @@
 export default () => (
-  <div style={{position: 'absolute', background: 'grey', width: 10, height: 10, bottom: 0, right: 0, opacity: 0.5}}></div>
+  <div style={{position: 'absolute', background: 'grey', width: 8, height: 8, bottom: 0, right: 0, opacity: 0.4}}></div>
 )
